75996a547cabab4d8fd619cd5075ddcc574ee7d1
Pho <pho.dmc5@gmail.com>
2025-02-04 18:38:13 +0000

    feat: Create function that returns "rock" "paper" or "scissors" randomly

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 console.log('Hello world');
+
+// todo - write computer choice logic
+function getComputerChoice(){
+    // generate random number
+    let random = Math.floor(Math.random() * 3) + 1
+ 
+    // return 'rock', 'paper', or 'scissors based on random
+    switch (random) {
+        case 1:
+            return 'rock'
+        case 2:
+            return 'paper'
+        default:
+            return 'scissors';
+    }
+}
