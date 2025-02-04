88d34f0c82e10849e3e64f96933066d12a097428
Pho <pho.dmc5@gmail.com>
2025-02-04 19:09:21 +0000

    feat: Create function to return human choice

--- script.js
+++ script.js
@@ -1,17 +1,30 @@
-console.log('Hello world');
+console.log("Hello world");
 
-// todo - write computer choice logic
-function getComputerChoice(){
-    // generate random number
-    let random = Math.floor(Math.random() * 3) + 1
- 
-    // return 'rock', 'paper', or 'scissors based on random
-    switch (random) {
-        case 1:
-            return 'rock'
-        case 2:
-            return 'paper'
-        default:
-            return 'scissors';
-    }
+function getComputerChoice() {
+  // generate random number
+  let random = Math.floor(Math.random() * 3) + 1;
+
+  // return 'rock', 'paper', or 'scissors based on random
+  switch (random) {
+    case 1:
+      return "rock";
+    case 2:
+      return "paper";
+    default:
+      return "scissors";
+  }
+}
+
+// todo - write human/player choice logic
+function getHumanChoice() {
+  let input = prompt("Enter choice: r for rock, p for paper & s for scissors");
+  console.log(`You chose ${input}`);
+
+  while (input != "r" && input != "p" && input != "s")
+    input = prompt("invalid input. Enter 'r', 'p' or 's'");
+
+
+  if (input == "r") return "rock";
+  if (input == "p") return "paper";
+  if (input == "s") return "scissors";
 }
