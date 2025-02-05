0eeb1c264100e6a9d741d766ffbfef01dcc9619a
Pho <pho.dmc5@gmail.com>
2025-02-05 10:42:54 +0000

    feat: create functions with logic to play a round and sets of rounds

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
 console.log("Hello world");
 
-// scores
-let humanScore = computerScore = 0
-
 function getComputerChoice() {
   // generate random number
   let random = Math.floor(Math.random() * 3) + 1;
@@ -18,11 +15,11 @@ function getComputerChoice() {
   }
 }
 
-// todo - write human/player choice logic
 function getHumanChoice() {
   let input = prompt("Enter choice: r for rock, p for paper & s for scissors");
   console.log(`You chose ${input}`);
 
+  // keep prompting until valid input entered
   while (input != "r" && input != "p" && input != "s")
     input = prompt("invalid input. Enter 'r', 'p' or 's'");
 
@@ -30,3 +27,35 @@ function getHumanChoice() {
   if (input == "p") return "paper";
   if (input == "s") return "scissors";
 }
+
+function playGame() {
+  // scores
+  let humanScore = 0,
+    computerScore = 0;
+
+  function playRound(humanChoice, computerChoice) {
+    humanChoice = humanChoice.toLowerCase();
+
+    if (humanChoice == computerChoice) {
+      console.log("Tough game. It's a draw");
+    } else if (
+      (humanChoice == "rock" && computerChoice == "scissors") ||
+      (humanChoice == "paper" && computerChoice == "rock") ||
+      (humanChoice == "scissors" && computerChoice == "paper")
+    ) {
+      humanScore++;
+      console.log(`Hurray! You win! ${humanChoice} beats ${computerChoice}`);
+    } else {
+      computerScore++;
+      console.log("Womp Womp... You lose");
+    }
+  }
+
+  for (let rounds = 0; rounds < 5; rounds++) {
+    playRound(getHumanChoice(), getComputerChoice())
+  }
+
+  console.log(`Human -> ${humanScore} ||==|| ${computerScore} <- Machine`)
+}
+
+playGame()
